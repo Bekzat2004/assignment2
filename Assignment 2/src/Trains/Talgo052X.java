a8770eb0c02e85676ec502cf3495f876784b561d
Bekzat2004 <121132251+Bekzat2004@users.noreply.github.com>
2023-01-21 16:15:39 +0600

    Delete Assignment 2 directory

--- Assignment 2/src/Trains/Talgo052X.java
+++
@@ -1,9 +0,0 @@
-package Trains;
-
-public class Talgo052X extends Lokomotiv {
-
-    public Talgo052X(String maxSpeed, int wagonTraction, String route) {
-        super(maxSpeed, wagonTraction, route);
-    }
-
-}
--- Assignment 2/src/Trains/Train058X.java
+++
@@ -1,7 +0,0 @@
-package Trains;
-
-public class Train058X extends Lokomotiv {
-    public Train058X(String maxSpeed, int wagonTraction, String route) {
-        super(maxSpeed, wagonTraction, route);
-    }
-}
